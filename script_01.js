fdab6a0d6891ec61ea3bc5d45465f1a5f29073b7
GoloMeyer84 <golomeyer84@googlemail.com>
2022-01-17 11:10:36 +0100

    BEISPIEL hinzugefügt

--- script_01.js
+++ script_01.js
@@ -53,4 +53,40 @@ test = true;    //bool
 console.log("Datentyp: " + typeof test); //zeigt den Datentyp von test an
 console.log("Inhalt: " + test); //zeigt den Inhalt von test an
 
+//03a Konstanten//
+const test2 = "HI"; //Deklaration und Wertzuweisung MUSS in einer Zeile erfolgen
+// test2 = 2; //Assignment auf eine Konstante wärend der Laufzeit nicht möglich!
+console.log(test2);
+
+// BEISPIEL //
+
+// Deklaration //
+
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark;
+
+let year = 2022 ; // kann als Variable angegeben werden
+// const year = 2022; // besser ist allerdings die Konstante (da sich der Wert nicht ändern soll)
+// let year = new Date(); //neuen Datentyp : Datum erzeugen
+// let year = date.getFullYear(); //volles Jahr aus dem Datum verwenden
+
+//Wertzuweisung//
+ageJohn = 25;
+ageMark = 30;
+
+//Berechnung//
+
+birthYearJohn = year - ageJohn;
+birthYearMark = year - ageMark;
+
+//Ausgabe
+console.log("Das Geburtsjahr von John ist " + birthYearJohn);
+console.log("Das Geburtsjahr von Mark ist " + birthYearMark );
+
+// BEISPIEL ENDE //
+
+
+
+
+
 
