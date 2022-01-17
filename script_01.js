4c8368db1af578f6015c9f684248309d8799b811
GoloMeyer84 <golomeyer84@googlemail.com>
2022-01-17 10:27:22 +0100

    Datentypen ergänzt

--- script_01.js
+++ script_01.js
@@ -2,7 +2,7 @@
 
 /***** Variablen 01 *******/
 
-// Zeilenkommentar//
+// // Zeilenkommentar//
 console.log("Zeilenkommentar");
 /*
 Block-
@@ -10,11 +10,11 @@ Kommentar
 */
 console.log("Blockkommentar");
 
-//Konsolenausgabe//
+// //Konsolenausgabe//
 
 console.log("Hallo Welt");
 
-//Ausgabe der Variable : firstName //
+// //Ausgabe der Variable : firstName //
 
 var firstName = "Thilo"
 
@@ -29,8 +29,28 @@ console.log(firstName2); // Ausgabe des Inhalts der Variable firstName2//
 let lastName = "Schönemeyer"; //Deklaration und Wertzuweisung//
 console.log(lastName); 
 
-//verknüpfen der Variablen firstName und lastName incl. Leerzeichen durch " "//
+// //verknüpfen der Variablen firstName und lastName incl. Leerzeichen durch " "//
 
 console.log(firstName + " " + lastName); 
+
+
+/* 03 Deklaration + Wertzuweisung II */
+
+// let firstName3, lastName2; //zusammenfassen von Deklarationen//
+// firstName3 = prompt("Bitte Vornamen eingeben"); //Eingabe firstName//
+// lastName2  = prompt("Bitte geben Sie den Familienname ein"); //Eingabe lastName//
+
+// console.log(firstName3 + " " + lastName2);
+
+//JS ist eine untypisierte Sprache (untyped)
+
+let test;
+test = "Hi";    //string
+test = 2;       //number
+test = true;    //bool
+//Inhalt der Variable test wird überschrieben (Datentyp wird geändert)
+
+console.log("Datentyp: " + typeof test); //zeigt den Datentyp von test an
+console.log("Inhalt: " + test); //zeigt den Inhalt von test an
 
 
