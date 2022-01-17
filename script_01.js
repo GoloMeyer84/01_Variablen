e2618ee82bef3a58110594dc583bef0f839ad0ee
GoloMeyer84 <golomeyer84@googlemail.com>
2022-01-17 09:20:06 +0100

    erste Variablenübungen hinzugefügt

--- script_01.js
+++ script_01.js
@@ -2,6 +2,35 @@
 
 /***** Variablen 01 *******/
 
+// Zeilenkommentar//
+console.log("Zeilenkommentar");
+/*
+Block-
+Kommentar
+*/
+console.log("Blockkommentar");
 
+//Konsolenausgabe//
+
+console.log("Hallo Welt");
+
+//Ausgabe der Variable : firstName //
+
+var firstName = "Thilo"
+
+console.log(firstName);
+
+/* 02 Deklaration + Wertzuweisung I */
+
+let firstName2; //Deklaration (Definition)//
+firstName2 = "Thilo2" //Wertzuweisung - Assignment //
+console.log(firstName2); // Ausgabe des Inhalts der Variable firstName2//
+
+let lastName = "Schönemeyer"; //Deklaration und Wertzuweisung//
+console.log(lastName); 
+
+//verknüpfen der Variablen firstName und lastName incl. Leerzeichen durch " "//
+
+console.log(firstName + " " + lastName); 
 
 
